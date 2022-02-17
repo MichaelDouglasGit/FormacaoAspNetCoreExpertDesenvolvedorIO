e7b9503f90cd869eb3bdefa20e34bd5ccc19f4d6
Michael Douglas <94067749+MichaelDouglasGit@users.noreply.github.com>
2022-02-17 12:16:41 -0300

    validação inicial sobre modelo Filme

--- DominandoAspNetMVCCore/MinhaDemoMVC/Controllers/HomeController.cs
+++ DominandoAspNetMVCCore/MinhaDemoMVC/Controllers/HomeController.cs
@@ -9,8 +9,6 @@ using MinhaDemoMVC.Models;
 
 namespace MinhaDemoMVC.Controllers
 {
-    [Route("")]
-    [Route("gestao-clientes")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
@@ -19,24 +17,35 @@ namespace MinhaDemoMVC.Controllers
         {
             _logger = logger;
         }
-
-        [Route("")]
-        [Route("pagina-inicial")]
-        [Route("pagina-inicial/{id:int}/{categoria:guid}")]
-        public IActionResult Index(int id, Guid categoria)
+        
+        public IActionResult Index()
         {
-            return View();
+            var filme = new Filme {
+                Titulo = "Oi",
+                DataLancamento = DateTime.Now,
+                Genero = null,
+                Avaliacao = 10,
+                Valor = 20000
+            };
+            return RedirectToAction("Privacy", filme);
+            // return View();
         }
 
-        [Route("privacidade")]
-        [Route("politica-de-privacidade")]
-        public IActionResult Privacy()
+        public IActionResult Privacy(Filme filme)
         {
+            if(ModelState.IsValid){
+                    
+            }
+
+            foreach(var error in ModelState.Values.SelectMany(m => m.Errors))
+            {
+                Console.WriteLine(error.ErrorMessage);
+            }
+
             return View();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        [Route("erro-encontrado")]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
--- DominandoAspNetMVCCore/MinhaDemoMVC/Models/Filme.cs
+++ DominandoAspNetMVCCore/MinhaDemoMVC/Models/Filme.cs
@@ -1,10 +1,12 @@
 using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+
 namespace MinhaDemoMVC.Models
 {
     public class Filme
     {
+        [Key]
         public int Id { get; set; }
         
         [Required(ErrorMessage = "O campo Título é obrigatório")]
@@ -17,14 +19,17 @@ namespace MinhaDemoMVC.Models
         public DateTime DataLancamento { get; set; }
         
         [RegularExpression(@"^[A-Z]+[a-zA-Z\u00C0-\u00FF""'\w-]*$", ErrorMessage = "Gênero em formato inválido")]
-        [StringLength(30, ErrorMessage ="Máximo de 30 caracteres")]
+        [StringLength(30, ErrorMessage ="Máximo de 30 caracteres"), Required(ErrorMessage = "O campo Gênero é obrigatório")]
         public string Genero { get; set; }
         
         [Range(1,1000, ErrorMessage = "Valor de 1 à 1000")]
         [Required(ErrorMessage = "Preencha o campo Valor")]
-        [Column]
+        [Column(TypeName = "decimal(18,2)")]
         public decimal Valor { get; set; }
 
-        public string Avaliacao { get; set; }
+        [RegularExpression(@"^[0-5]*$", ErrorMessage = "Somente números")]
+        [Required(ErrorMessage = "Preencha o campo Avaliação")]
+        [Display(Name = "Avaliação")]
+        public int Avaliacao { get; set; }
     }
 }
