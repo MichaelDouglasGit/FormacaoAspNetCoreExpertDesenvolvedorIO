0893c6b7ce8111b28e4de9aa9186bdbb5ae2adf6
Michael Douglas <94067749+MichaelDouglasGit@users.noreply.github.com>
2022-02-21 22:16:06 -0300

    tagHelper Email criada e implementada

--- DominandoAspNetMVCCore/PrimeiraAppMvcCore/AppModelo/src/DevIO.UI.Site/obj/Debug/netcoreapp3.1/Razor/Views/Home/Index.cshtml.g.cs
+++ DominandoAspNetMVCCore/PrimeiraAppMvcCore/AppModelo/src/DevIO.UI.Site/obj/Debug/netcoreapp3.1/Razor/Views/Home/Index.cshtml.g.cs
@@ -20,7 +20,7 @@ using DevIO.UI.Site;
 #line hidden
 #nullable disable
     [global::Microsoft.AspNetCore.Razor.Hosting.RazorSourceChecksumAttribute(@"SHA1", @"1076dbe74af481aea7eae5142b3ce9af92c19488", @"/Views/Home/Index.cshtml")]
-    [global::Microsoft.AspNetCore.Razor.Hosting.RazorSourceChecksumAttribute(@"SHA1", @"34ce4a00e79d1c79e8566ec46ce0146975263ee2", @"/Views/_ViewImports.cshtml")]
+    [global::Microsoft.AspNetCore.Razor.Hosting.RazorSourceChecksumAttribute(@"SHA1", @"d422f58880ba81cb40380a31cef49b3445cddf72", @"/Views/_ViewImports.cshtml")]
     public class Views_Home_Index : global::Microsoft.AspNetCore.Mvc.Razor.RazorPage<dynamic>
     {
         #pragma warning disable 1998
