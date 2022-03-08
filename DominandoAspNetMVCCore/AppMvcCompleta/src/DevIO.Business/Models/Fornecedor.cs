6d482a1bd9b5c0a352a2c20d636abab63686fe40
Michael Douglas <94067749+MichaelDouglasGit@users.noreply.github.com>
2022-03-08 13:58:26 -0300

    Projeto "AppMvcCompleta" criado e ambiente inicial configurado

---
+++ DominandoAspNetMVCCore/AppMvcCompleta/src/DevIO.Business/Models/Fornecedor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppMvcBasica.Models
+{
+    public class Fornecedor : Entity
+    {
+
+        public string Nome { get; set; }
+        public string Documento { get; set; }
+        public TipoFornecedor TipoFornecedor { get; set; }
+        public Endereco Endereco { get; set; }        
+        public bool Ativo { get; set; }
+
+        /* EF Relations*/
+        public IEnumerable<Produto> Produtos { get; set; }
+    }
+}
